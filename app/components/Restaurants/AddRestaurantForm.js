a5d54b1ec276169c64e719e4269060c4cd9ad7a1
Raul Chihuaicura <ch.raul01@gmail.com>
2021-07-12 22:15:03 -0400

    Guardando datos de locales veganos en los estados

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, ScrollView, Alert, Dimensions } from "react-native";
 import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
 
-export default function AddRestaurantForm() {
+export default function AddRestaurantForm(props) {
+  const { toastRef, setIsLoading, navigation } = props;
+  const [restaurantName, setRestaurantName] = useState("");
+  const [restaurantAddress, setRestaurantAddress] = useState("");
+  const [restaurantDescription, setRestaurantDescription] = useState("");
+
+  const addRestaurant = () => {
+    console.log("OK");
+    console.log("restaurantName: " + restaurantName);
+    console.log("restaurantAddress: " + restaurantAddress);
+    console.log("restaurantDescription: " + restaurantDescription);
+  };
   return (
     <ScrollView style={styles.scrollView}>
-      <FormAdd />
+      <FormAdd
+        setRestaurantName={setRestaurantName}
+        setRestaurantAddress={setRestaurantAddress}
+        setRestaurantDescription={setRestaurantDescription}
+      />
+      <Button
+        title="Crear Local Vegano"
+        onPress={addRestaurant}
+        buttonStyle={styles.btnAddRestaurant}
+      />
     </ScrollView>
   );
 }
 
 function FormAdd(props) {
+  const { setRestaurantName, setRestaurantAddress, setRestaurantDescription } =
+    props;
+
   return (
     <View style={styles.viewForm}>
       <Input
         placeholder="Nombre de Local Vegano"
         containerStyle={styles.input}
+        onChange={(e) => setRestaurantName(e.nativeEvent.text)}
       />
-      <Input placeholder="Dirección" containerStyle={styles.input} />
+      <Input
+        placeholder="Dirección"
+        containerStyle={styles.input}
+        onChange={(e) => setRestaurantAddress(e.nativeEvent.text)}
+      />
       <Input
         placeholder="Descripción del local vegano"
         multiline={true}
         inputContainerStyle={styles.textArea}
+        onChange={(e) => setRestaurantDescription(e.nativeEvent.text)}
       />
     </View>
   );
@@ -44,5 +73,9 @@ const styles = StyleSheet.create({
     width: "100%",
     padding: 0,
     margin: 0,
+  },
+  btnAddRestaurant: {
+    backgroundColor: "#C2A0E8",
+    margin: 20,
   },
 });
