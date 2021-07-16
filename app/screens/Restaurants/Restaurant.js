1d47aa6a1977a96f7d0ec8629e9c401ce9635855
Raul Chihuaicura <ch.raul01@gmail.com>
2021-07-15 23:40:00 -0400

    Agregando carrusel con las imagenes del local

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import Loading from "../../components/Loading";
+import Carousel from "../../components/Carousel";
+
 import { firebaseApp } from "../../utils/firebase";
 import firebase from "firebase/app";
 import "firebase/firestore";
 
 const db = firebase.firestore(firebaseApp);
+const screenWidth = Dimensions.get("window").width;
 
 export default function Restaurant(props) {
   const { navigation, route } = props;
@@ -25,11 +29,22 @@ export default function Restaurant(props) {
       });
   }, []);
 
+  if (!restaurant) return <Loading isVisible={true} text="Cargando..." />;
+
   return (
-    <View>
-      <Text>Local info ...</Text>
-    </View>
+    <ScrollView vertical style={styles.viewBody}>
+      <Carousel
+        arrayImages={restaurant.images}
+        height={250}
+        width={screenWidth}
+      />
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+    backgroundColor: "#DED7FA",
+  },
+});
