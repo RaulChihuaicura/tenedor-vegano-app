c4e60c74078c699f51a0f768e8722f739aee2c9e
Raul Chihuaicura <ch.raul01@gmail.com>
2021-07-16 13:53:07 -0400

    Guardando los datos del formulario en cada estado

--- app/screens/Restaurants/AddReviewRestaurant.js
+++ app/screens/Restaurants/AddReviewRestaurant.js
@@ -1,35 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { AirbnbRating, Button, Input } from "react-native-elements";
 
 export default function AddReviewRestaurant(props) {
   const { navigation, route } = props;
   const { idRestaurant } = route.params;
+  const [rating, setRating] = useState(null);
+  const [title, setTitle] = useState("");
+  const [review, setReview] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const addReview = () => {
+    console.log("rating: ", rating);
+    console.log("title: ", title);
+    console.log("review: ", review);
+  };
 
   return (
-    <View style={styles.viewBody}>
+    <ScrollView style={styles.viewBody}>
       <View style={styles.viewRating}>
         <AirbnbRating
           count={5}
           reviews={["Pésimo", "Deficiente", "Normal", "Muy bueno", "Excelente"]}
           defaultRating={0}
           size={35}
+          onFinishRating={(value) => {
+            setRating(value);
+          }}
         />
       </View>
       <View style={styles.formReview}>
-        <Input placeholder="Título" containerStyle={styles.input} />
+        <Input
+          placeholder="Título"
+          containerStyle={styles.input}
+          onChange={(e) => setTitle(e.nativeEvent.text)}
+        />
         <Input
           placeholder="Comentario..."
           multiline={true}
           inputContainerStyle={styles.textArea}
+          onChange={(e) => setReview(e.nativeEvent.text)}
         />
         <Button
           title="Enviar comentario"
           containerStyle={styles.btnContainer}
           buttonStyle={styles.btn}
+          onPress={addReview}
         />
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
