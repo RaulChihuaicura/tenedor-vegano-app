459add09038cff7788de2be775677d78d3c6a17c
Raul Chihuaicura <ch.raul01@gmail.com>
2021-07-15 23:02:44 -0400

    Obteniendo informacion del local

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Restaurant(props) {
   const { navigation, route } = props;
   const { id, name } = route.params;
+  const [restaurant, setRestaurant] = useState(null);
+  console.log(restaurant);
 
   navigation.setOptions({ title: name });
+
+  useEffect(() => {
+    db.collection("restaurants")
+      .doc(id)
+      .get()
+      .then((response) => {
+        const data = response.data();
+        data.id = response.id;
+        setRestaurant(data);
+      });
+  }, []);
+
   return (
     <View>
       <Text>Local info ...</Text>
