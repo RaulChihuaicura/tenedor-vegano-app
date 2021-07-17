b87811e08f639578e02fd33c48d67637ae287b7b
Raul Chihuaicura <ch.raul01@gmail.com>
2021-07-17 00:16:21 -0400

    Eliminar local de la lista de favoritos

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -82,7 +82,25 @@ export default function Restaurant(props) {
   };
 
   const removeFavorite = () => {
-    console.log("Eliminar de favoritos");
+    db.collection("favorites")
+      .where("idRestaurant", "==", restaurant.id)
+      .where("idUser", "==", firebase.auth().currentUser.uid)
+      .get()
+      .then((response) => {
+        response.forEach((doc) => {
+          const idFavorite = doc.id;
+          db.collection("favorites")
+            .doc(idFavorite)
+            .delete()
+            .then(() => {
+              setIsFavorite(false);
+              toastRef.current.show("Local eliminado de favoritos");
+            })
+            .catch(() => {
+              toastRef.current.show("Error al eliminar el local de favoritos");
+            });
+        });
+      });
   };
 
   if (!restaurant) return <Loading isVisible={true} text="Cargando..." />;
