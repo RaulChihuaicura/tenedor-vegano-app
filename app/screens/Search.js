6d71975bf4fd89fcefefef8fc488c44155608ca6
Raul Chihuaicura <ch.raul01@gmail.com>
2021-07-17 11:23:24 -0400

    Implementando barra de busqueda

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, FlatList, Image } from "react-native";
+import { SearchBar, ListItem, Icon } from "react-native-elements";
 
-export default function Search() {
+export default function Search(props) {
+  const { navigation } = props;
+  const [search, setSearch] = useState("");
   return (
-    <View>
-      <Text>Search...</Text>
+    <View style={styles.viewBody}>
+      <SearchBar
+        placeholder="Busca un local vegano..."
+        onChangeText={(e) => setSearch(e)}
+        value={search}
+        containerStyle={StyleSheet.searchBar}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  viewBody: {
+    backgroundColor: "#DED7FA",
+  },
+  searchBar: {
+    marginBottom: 20,
+  },
+});
