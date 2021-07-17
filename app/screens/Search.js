124cde556d0e474f706a4cb2210a1f73b0b3f372
Raul Chihuaicura <ch.raul01@gmail.com>
2021-07-17 11:37:25 -0400

    Ejecutando la busqueda contra firestore usando la libreria FireSQL

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, FlatList, Image } from "react-native";
 import { SearchBar, ListItem, Icon } from "react-native-elements";
+import { FireSQL } from "firesql";
+import firebase from "firebase/app";
+
+const fireSQL = new FireSQL(firebase.firestore(), { includeId: "id" });
 
 export default function Search(props) {
   const { navigation } = props;
   const [search, setSearch] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+  console.log(restaurants);
+
+  //uso de la lobreria fireSQL para hacer sintaxis SQL en firestore
+  useEffect(() => {
+    if (search) {
+      fireSQL
+        .query(`SELECT * FROM restaurants WHERE name LIKE '${search}%'`)
+        .then((response) => {
+          setRestaurants(response);
+        });
+    }
+  }, [search]);
+
   return (
     <View style={styles.viewBody}>
       <SearchBar
